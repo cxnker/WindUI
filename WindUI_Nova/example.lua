85339094e21579c5d4011dc2e62b894fb9c5d025
Cxnker <72629137+cxnker@users.noreply.github.com>
2026-03-03 15:51:44 -0400

    Update example.lua

--- WindUI_Nova/example.lua
+++ WindUI_Nova/example.lua
@@ -1,7 +1,10 @@
 local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/cxnker/WindUI/refs/heads/main/WindUI_Nova/windlib.lua"))()
 
 local Window = WindUI:CreateWindow({
-    Title = "Mi Ventana",
-    Author = "Yo",
-    Size = UDim2.fromOffset(600, 500)
+    Title = "Prueba",
+    -- Author = "Yo",
+    Size = UDim2.fromOffset(500, 400)
 })
+    
+local Tab = Window:Tab({ Title = "Test" })
+Tab:Button({ Title = "Click", Callback = function() print("Funciona!") end })
