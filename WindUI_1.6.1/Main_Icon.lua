555f72184df71d81f77a9e88841b1841b2dc8b72
Cxnker <72629137+cxnker@users.noreply.github.com>
2026-03-03 18:47:56 -0400

    Update Icons source URL in Main_Icon.lua

--- WindUI_1.6.1/Main_Icon.lua
+++ WindUI_1.6.1/Main_Icon.lua
@@ -2,7 +2,7 @@
 -- https://raw.githubusercontent.com/Footagesus/Icons/refs/heads/main/lucide/dist/Icons.lua
 
 local Icons = {
-    ["lucide"] = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Footagesus/Icons/refs/heads/main/lucide/dist/Icons.lua"))()
+    ["lucide"] = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/cxnker/WindUI/refs/heads/main/WindUI_1.6.1/lucide.lua"))()
     
     
     -- More soon 
